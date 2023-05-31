f17b5162b4479f204a3cbfeaf6f636a2c8e23ca4
danilopiccinni <danilopiccinni@gmail.com>
2023-05-31 18:50:10 +0200

    bonus filtraggio

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,16 +4,38 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
-        $projects = Project::with('type', 'technologies')->paginate(2);
+    public function index(Request $request) {
 
+        $requestData = $request->all();
+
+        $types = Type::all();
+
+        if($request->has('type_id') && $requestData['type_id']) {
+            
+            $projects = Project::where('type_id' , $requestData['type_id'])
+                ->with('type', 'technologies')
+                ->get();
+
+            if(count($projects) == 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'nessun pos corrispondente'
+                ]);
+            }
+        } else {
+            
+            $projects = Project::with('type', 'technologies')->get();
+        }
+        
         return response()->json([
             'success' => true,
-            'results' => $projects
+            'results' => $projects,
+            'types' => $types,
         ]);
     }
 
